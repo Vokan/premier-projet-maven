3cddf7bb93fc16a14875ead647e5efe170740dbb
Vokan Kocak <vokankocak@gmail.com>
2020-06-13 19:06:42 +0200

    Etape7 tp 1

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,4 +1,6 @@
 package dev;
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -6,8 +8,13 @@ package dev;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
-    }
+       // System.out.println( "Hello World!" );
+    
+	// On utilise par default font  standard.flf qui provient de maven artifact.
+
+     String asciiArt = FigletFont.convertOneLine("hello");
+     System.out.println(asciiArt1);
+}
 }
